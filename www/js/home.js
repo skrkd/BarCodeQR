fe56c9b8fd6ad19236aea92f83926691880897d6
Sanketkumar Dave <Sanketkumar.Dave@logstarerp.com>
2021-04-03 16:38:33 +0530

    Added Copy to Clipboard button to copy scan data

--- www/js/home.js
+++ www/js/home.js
@@ -2,6 +2,17 @@
 app.controller('homeController', ['$scope', '$http', function ($scope, $http) {
     $(document).ready(function () {
         $scope.hideData();
+
+        $('.js-tooltip').tooltip();
+
+        // Copy to clipboard
+        // Grab any text in the attribute 'data-copy' and pass it to the 
+        // copy function
+        $('.js-copy').click(function () {
+            var text = $(this).attr('data-copy');
+            var el = $(this);
+            copyToClipboard(text, el);
+        });
     });
     $scope.showData = function () {
         $scope.hideButton = true;
@@ -17,6 +28,7 @@ app.controller('homeController', ['$scope', '$http', function ($scope, $http) {
     $scope.barCodeQRScan = function () {
         //alert("Scanning is comming soon!!!");
         cordova.plugins.barcodeScanner.scan(function (result) {
+            $scope.scannedData = JSON.stringify(result);// result;
             $scope.scannedText = result.text;
             $scope.scannedFormat = result.format;
             if (result.cancelled == false) {
@@ -54,4 +66,34 @@ app.controller('homeController', ['$scope', '$http', function ($scope, $http) {
         document.getElementById("mySidebar").style.width = "0";
         document.getElementById("main").style.marginLeft = "0";
     };
+
+    // COPY TO CLIPBOARD
+    // Attempts to use .execCommand('copy') on a created text field
+    // Falls back to a selectable alert if not supported
+    // Attempts to display status in Bootstrap tooltip
+    // ------------------------------------------------------------------------------
+
+    function copyToClipboard(text, el) {
+        var copyTest = document.queryCommandSupported('copy');
+        var elOriginalText = el.attr('data-original-title');
+
+        if (copyTest === true) {
+            var copyTextArea = document.createElement("textarea");
+            copyTextArea.value = text;
+            document.body.appendChild(copyTextArea);
+            copyTextArea.select();
+            try {
+                var successful = document.execCommand('copy');
+                var msg = successful ? 'Copied!' : 'Whoops, not copied!';
+                el.attr('data-original-title', msg).tooltip('show');
+            } catch (err) {
+                console.log('Oops, unable to copy');
+            }
+            document.body.removeChild(copyTextArea);
+            el.attr('data-original-title', elOriginalText);
+        } else {
+            // Fallback if browser doesn't support .execCommand('copy')
+            window.prompt("Copy to clipboard: Ctrl+C or Command+C, Enter", text);
+        }
+    }
 }]);
